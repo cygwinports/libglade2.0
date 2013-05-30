c5896c86b62a93af8f73129641b8ba7d7eaa1531
Yaakov Selkowitz <yselkowitz@users.sourceforge.net>
2013-05-30 18:11:34 -0500

    Fix gtk-doc build

--- postinstall.sh
+++ postinstall.sh
@@ -1,4 +1,4 @@
-xmlcatalog --noout --add "system" \
+/usr/bin/xmlcatalog --noout --add "system" \
 	"http://glade.gnome.org/glade-2.0.dtd" \
 	/usr/share/xml/libglade/glade-2.0.dtd \
 	/etc/xml/catalog
